673b262bf6a140997f18861e35809e834eca19cd
ikedamuneki <muneki.ikeda@ucsf.edu>
2023-09-15 21:45:06 -0700

    first commit

--- example.py
+++ example.py
@@ -3,6 +3,7 @@
 
 from eats_worm import *
 
+<<<<<<< Updated upstream
 #data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numZ = 157; endT = 2; pixelSize = [0.75, 0.24, 0.20]
 #data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale"; numZ = 127; endT = 10; pixelSize = [1.42, 0.37, 0.32]
 ## pixelSize = [0.35, 0.75, 0.3545×sin(π×Θ°/180°)]
@@ -10,6 +11,27 @@ from eats_worm import *
 #data_directory = "E:/SCAPE/Data/tiff_stacks/20230908_GreenBeads4um_run2/20230908_GreenBeads4um_run2"; numZ = 200; endT = 3; pixelSize = [1.8, 1, 1.16]
 #data_directory = "E:/SCAPE/Data/20230911/tiff_stacks/20230911_GreenBeads4um_run3/Deskewed_-60"; numZ = 200; endT = 3; pixelSize = [0.55, 1, 0.47]
 data_directory = "E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230913/tiff_stacks/20230913_OH16290_3_run1/Deskewed_-45"; numZ = 200; endT = 10; pixelSize = (0.55, 1, 0.47)
+=======
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numZ = 157; endT = 360; pixelSize = [0.75, 0.24, 0.20]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale"; numZ = 127; endT = 100; pixelSize = [1.42, 0.37, 0.32]
+#data_directory = "Z:/muneki/MATLAB/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numZ = 157; endT = 3575; pixelSize = [0.75, 0.24, 0.20]
+## pixelSize = [0.35, 0.75, 0.3545×sin(π×Θ°/180°)]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230522_OH16290_1_run2"; numZ = 199; endT = 1039; pixelSize = [0.35, 0.75, 0.29]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230529_OH16290_1_run1"; numZ = 127; endT = 1039; pixelSize = [0.35, 0.75, 0.29]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230529_OH16290_2_run1"; numZ = 127; endT = 985; pixelSize = [0.35, 0.75, 0.29]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230711_OH16290_1_run1"; numZ = 127; endT = 1688; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230714_OH16290_1_run1"; numZ = 127; endT = 1685; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230715_OH16290_1_run1"; numZ = 127; endT = 1684; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230716_OH16290_1_run1"; numZ = 127; endT = 1682; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230719_OH16290_1_run1"; numZ = 120; endT = ; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230720_OH16290_1_run1"; numZ = 120; endT = 1714; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230721_OH16290_1_run1"; numZ = 120; endT = 1713; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230722_OH16290_1_run1"; numZ = 120; endT = 1712; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230723_OH16290_1_run1"; numZ = 120; endT = 1715; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230723_OH16290_1_run2"; numZ = 120; endT = 1715; pixelSize = [0.35, 0.75, 0.25]
+#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230724_OH16290_1_run1"; numZ = 120; endT = 1715; pixelSize = [0.35, 0.75, 0.25]
+data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230726_OH16290_1_run2"; numZ = 120; endT = 1715; pixelSize = [0.35, 0.75, 0.25]
+>>>>>>> Stashed changes
 output_directory = data_directory + "/output/"
 
 
@@ -31,9 +53,15 @@ arguments = {
         "window_size": 5, #3
         "min_distance": 11, #1
         "manage_collisions": "prune",
+<<<<<<< Updated upstream
         "fb_threshold_margin": 50 #10
     },
     "register_frames": False, #True
+=======
+        "fb_threshold_margin": 50
+    },
+    "register_frames": True,
+>>>>>>> Stashed changes
     "output_dir": output_directory
 }
 
--- noblob.py
+++ noblob.py
@@ -4,9 +4,14 @@
 from eats_worm import *
 
 ## pixelSize = [0.35, 0.75, 0.3545×sin(π×Θ°/180°)]
+<<<<<<< Updated upstream
 #m = MultiFileTiff('E:/SCAPE/Data/tiff_stacks/20230913_OH16290_1_run2/Deskewed_-45', numz=200, numc=1, anisotropy=(0.55, 1, 0.47))
 #m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230914/tiff_stacks/20230914_OH16290_3_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.55, 0.5, 0.47))
 m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/GreenBeads/tiff_stacks/runA_run1/09-15-15-54', numz=200, numc=1, anisotropy=(1, 0.55, 0.47))
+=======
+#m = MultiFileTiff('C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/HiCAM_2000/165mm/GreenBeads/20230911_GreenBeads4um_run3', numz=200, numc=1, anisotropy=(0.55, 1, 0.47))
+m = MultiFileTiff('Z:/muneki/MATLAB/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230913_OH16290_2_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.55, 1, 0.47))
+>>>>>>> Stashed changes
 
 viewer = napari.Viewer(ndisplay=3)
 viewer.add_image(m.get_dask_array(), scale=m.anisotropy)
